0108f3a7063714af1eddf347882abe3102eb2160
Ryan Baxley <ryan.baxley1@gmail.com>
2017-01-21 15:54:22 -0500

    Fixed hanging on bad url

---
+++ jobSearch.js
@@ -0,0 +1,163 @@
+//const indeed = require('indeed-scraper');
+const indeed = require('../indeed-scraper/index.js');
+const fs = require('fs');
+const request = require('request');
+const nodemailer = require('nodemailer');
+const ini = require('ini');
+const emailConfig = ini.parse(fs.readFileSync('./config/email.ini', 'utf-8'))
+
+// Keywords need to be lowercase
+const keywords = [
+  {key: 'junior', value: 5},
+  {key: 'entry', value: 5},
+  {key: 'recent graduate', value: 5},
+  {key: 'fresh graduate', value: 5},
+  {key: 'developer', value: 5},
+  {key: 'software engineer', value: 5},
+  {key: 'programmer', value: 5},
+  {key: 'javascript', value: 3},
+  {key: 'node js', value: 3},
+  {key: 'node.js', value: 3},
+  {key: 'angular', value: 3},
+  {key: 'android', value: 3},
+  {key: 'sql', value: 1},
+  {key: 'java', value: 1},
+  {key: 'c++', value: 1},
+  {key: 'python', value: 1}
+];
+
+/* Get all the jobs for an array of cities */
+function getAllJobs(){
+  const cities = [];
+  cities.push(queryPromise('Software', 'Atlanta, GA',         25, 'entry_level', 2));
+  cities.push(queryPromise('Software', 'Raleigh-Durham, NC',  25, 'entry_level', 2));
+  cities.push(queryPromise('Software', 'Charlotte, NC',       25, 'entry_level', 2));
+  cities.push(queryPromise('Software', 'Clemson, SC',         50, 'entry_level', 2));
+  cities.push(queryPromise('Software', 'Charleston, SC',      25, 'entry_level', 2));
+  return Promise.all(cities);
+}
+
+function queryPromise(query, city, radius, level, maxAge){
+  return new Promise((resolve, reject) => {
+    indeed.query({query: query, city: city, radius: radius, level: level, maxAge: maxAge})
+      .then(res => resolve(res))
+      .catch(err => {
+        console.log('Error for ' + city + ': ' + err);
+        resolve([]);
+      });
+  });
+}
+
+function createHtmlPage(cities){
+  console.log('cities: ', cities);
+  return new Promise((resolve, reject) => {
+    const fileName = 'jobs.html';
+    sortJobs(cities, keywords).then(jobs => {
+      const fd = fs.openSync(fileName, 'w');
+      const d = new Date();
+      const todayString = (d.getMonth()+1) + '-' + d.getDate();
+      fs.appendFileSync(fd, '<!DOCTYPE html> <html><header><h1>Jobs for ' + todayString + '</h1></header><body>');
+      jobs.forEach(e => generateJobHtml(e, fd));
+      fs.appendFileSync(fd, '</body></html>');
+      fs.closeSync(fd);
+      resolve(fileName);
+    });
+  });
+}
+
+function sortJobs(cities, keywords){
+  return new Promise((resolve, reject) => {
+    const jobs = [].concat.apply([], cities);
+    const jobInfoPromises = jobs.map(e => getJobInfo(e));
+    Promise.all(jobInfoPromises).then( jobsInfo => {
+      console.log('jobsInfo: ', jobsInfo);
+      jobsInfo.forEach((jobInfo, i, a) => {
+        const score = getKeyScore(jobInfo, keywords);
+        jobs[i].score = score;
+      });
+      const sorted = jobs.sort((a, b) => b.score - a.score);
+      const filtered = jobs.filter(e => e.score > -1);
+      resolve(filtered);
+    });
+  });
+}
+
+function getKeyScore(jobInfo, keywords){
+  console.log('\n\nGetting score of ' + jobInfo.title);
+  const jobInfoLC = {
+    title: jobInfo.title.toLowerCase(), 
+    body: jobInfo.body.toLowerCase()
+  };
+  return keywords.reduce((s, e) => {
+    let v = 0;
+    if(jobInfoLC.title.includes(e.key)){
+      v += e.value * 3;
+      console.log('Found ' + e.key + ' in title ' + e.value * 3);
+    }
+    if(jobInfoLC.body.includes(e.key)){
+      v += e.value;
+      console.log('Found ' + e.key + ' in body ' + e.value);
+    }
+    return s + v;
+  }, 0);
+}
+
+function getJobInfo(job){
+  return new Promise((resolve, reject) => {
+    console.log('about to request: ' + job.url);
+    request({url: job.url, timeout: 1500}, (error, response, body) => {
+      // If we can load the page add that to the info,
+      // otherwise just use the title as the info
+      if (!error) {
+        resolve({title: job.title, body: body + job.summary});
+      }else{
+        resolve({title: job.title, body: job.summary});
+      }
+    });
+  });
+}
+
+function generateJobHtml(job, fd){
+  let html = '';
+  html += '<hr>';
+  html += '<p>';
+  html += '<h3>';
+  html += '<a href="' + job.url + '">' + job.title + '</a>';
+  html += ' | ' + job.score;
+  html += '</h3>';
+  html += '<h4>' + job.company + ' (' + job.location + ')' + '</h4>';
+  html += '<p>' + job.summary + '</p>';
+  html += '</p>';
+  fs.appendFileSync(fd, html);
+}
+
+function sendEmail(fileName){
+  const msg = fs.readFileSync(fileName);
+
+  const from = emailConfig.from;
+  const pass = emailConfig.password;
+  const to = emailConfig.to;
+  const transporter = nodemailer.createTransport('smtps://'+from+':'+pass+'@smtp.gmail.com');
+
+  console.log('Attempting to send email from ' + from + ' to ' + to);
+  
+  const d = new Date();
+  const todayString = (d.getMonth()+1) + '-' + d.getDate();
+
+  const mailOptions = {
+    from: from, // sender address
+    to: to, // list of receivers
+    subject: 'Indeed Jobs for ' + todayString, // Subject line
+    html: msg // plaintext body
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+      return console.log(error);
+    }
+    console.log('Jobs email sent: ' + info.response);
+  });
+}
+
+/* The entry point */
+
+getAllJobs().then(createHtmlPage).then(sendEmail);
